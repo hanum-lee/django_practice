795820e8c84e9808fca378e6e53ab4833c98a466
hanum-lee <chococloudy@gmail.com>
2020-06-24 02:22:14 -0600

    Game Testing html added. path: polls/gametest

--- game/views.py
+++ game/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Context,loader
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    template = loader.get_template("game/index.html")
-    return HttpResponse(template.render())
+    
+    return render(request,'game/index.html')
--- polls/views.py
+++ polls/views.py
@@ -33,3 +33,6 @@ def results(request,question_id):
 def vote(request,question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+def gametest(request):
+    return render(request,'polls/gametest.html')
+
